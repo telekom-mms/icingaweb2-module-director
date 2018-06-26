0e2cfe97dad4f1fcb2deda56ab03d6830f6850c2
Martin Neubert <martin.neubert@t-systems.com>
2018-06-26 09:19:19 +0200

    allow handling of serviceapplies via the cli

---
+++ application/clicommands/ServiceapplyCommand.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Icinga\Module\Director\Clicommands;
+
+/**
+ * Manage Icinga Services Applies
+ *
+ * Use this command to show, create, modify or delete Icinga Service Applies
+ * objects
+ */
+class ServiceapplyCommand extends ServiceCommand {
+
+}
